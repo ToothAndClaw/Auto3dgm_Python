Fixed issue where scaling information was not properly saved

Auto3dgm_Python.py
@@ -318,16 +318,14 @@ class interface:
         numFail=0
         for name, mesh in ss_res[num_subsample[0]]['output'].items():
             mesh.name = name
-            newMesh = MeshFactory.mesh_from_data(mesh.koodinimi, center_scale=True, name=mesh.name)
-            low_res_meshes.append(newMesh)  
-            
+            newMesh = MeshFactory.mesh_from_data(mesh.koodinimi, center_scale=True, name=mesh.name) 
+            low_res_meshes.append(newMesh)
 
         self.sampledMeshes = []
         for name, mesh in ss_res[num_subsample[1]]['output'].items():
             mesh.name = name
             newMesh = MeshFactory.mesh_from_data(mesh.koodinimi, center_scale=True, name=mesh.name)
             self.sampledMeshes.append(newMesh)  
-
         sample_time = time.time() - sample_time
         print("--- %s seconds for sampling meshes ---" % (sample_time),flush=True)
         print("Finished sampling",flush=True)
@@ -444,11 +442,13 @@ class interface:
             lmtranspose = V.T @ perm
             #landmarks = scaledV
             landmarks = np.transpose(np.matmul(rot,lmtranspose))
+            print(mesh.initial_scale)
+            unscaledLandmarks = landmarks*mesh.initial_scale
             if not origScale:
                 mesh = auto3dgm_nazar.mesh.meshfactory.MeshFactory.mesh_from_data(vertices=landmarks,name=mesh.name, center_scale=False, deep=True)
                 meshes.append(mesh)
             else:
-                mesh = auto3dgm_nazar.mesh.meshfactory.MeshFactory.mesh_from_data(vertices=landmarks,name=mesh.name, center_scale=True, deep=True)
+                mesh = auto3dgm_nazar.mesh.meshfactory.MeshFactory.mesh_from_data(vertices=unscaledLandmarks,name=mesh.name, center_scale=False, deep=True)
                 meshes.append(mesh)
 
         return(meshes)

auto3dgm_nazar/mesh/mesh.py
@@ -71,13 +71,14 @@ class Mesh:
         return np.linalg.norm(self.vertices, 'fro')
 
     def center(self):
-        transform = vtk.vtkTransform()
-        transform.Translate(-self.centroid[0], -self.centroid[1], -self.centroid[2])
-        transformt = vtk.vtkTransformPolyDataFilter()
-        transformt.SetInputData(self.polydata)
-        transformt.SetTransform(transform)
-        transformt.Update()
-        self.polydata = transformt.GetOutput()
+        transform = tvtk.Transform()
+        transform.translate(-self.centroid[0], -self.centroid[1], -self.centroid[2])
+        transformt = tvtk.TransformPolyDataFilter()
+        transformt.set_input_data(self.polydata)
+        transformt.transform= transform
+        transformt.update()
+        self.polydata = transformt.get_output()
+        self.koodinimi = self.polydata.points.to_array()
 
     def scale_unit_norm(self):
         transform = tvtk.Transform()

auto3dgm_nazar/mesh/meshfactory.py
@@ -73,7 +73,6 @@ class MeshFactory(object):
             vertices = vertices-np.matlib.repmat(np.mean(vertices,axis=0),numV,1)
             #print(vertices[0,:])
             #print(np.mean(vertices,axis=0))
-            vertices = vertices/np.linalg.norm(vertices,'fro')
         faces = array(faces, dtype=int)
         vertices = array(vertices,dtype=float)
 
@@ -96,7 +95,9 @@ class MeshFactory(object):
             #cells.SetCells(nf, vtk_id_array)
             #polydata.SetPolys(cells)
         #polydata=tvtk.to_tvtk(polydata)
-        return Mesh(vtk_mesh=polydata, center_scale=False, name=name)
+        finalMesh = Mesh(vtk_mesh=polydata, center_scale=center_scale, name=name)
+        
+        return finalMesh
 
     @staticmethod
     def off_parser(file_path):